0b93a6d53d4942a5f95d074f965bdb148ed994a1
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-04-04 20:00:45 +0300

    Add password validation

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import emailValidation from "./modules/emailValidation.js";
 import coutryValidation from "./modules/countryValidation.js";
 import zipCodeValidation from "./modules/zipCodeValidation.js";
+import passwordValidation from "./modules/passwordValidation.js";
 
 const app = () => {
   emailValidation();
   coutryValidation();
   zipCodeValidation();
+  passwordValidation();
 };
 
 export default app;
---
+++ src/modules/passwordValidation.js
@@ -0,0 +1,37 @@
+const form = document.getElementById("form");
+const passwordInput = document.getElementById("password");
+const passwordError = document.getElementById("password-error");
+
+const passwordValidation = () => {
+  passwordInput.addEventListener("input", (event) => {
+    console.log("password-value:", event.target.value);
+    if (passwordInput.validity.valid) {
+      passwordError.textContent = "";
+      passwordError.classList.remove("active");
+    } else {
+      showError();
+    }
+  });
+
+  form.addEventListener("submit", (event) => {
+    if (!passwordInput.validity.valid) {
+      showError();
+      event.preventDefault();
+    }
+  });
+
+  const showError = () => {
+    if (passwordInput.validity.valueMissing) {
+      passwordError.textContent = "You need to enter an password.";
+    } else if (passwordInput.validity.tooShort) {
+      passwordError.textContent = `The password needs to be between ${passwordInput.minLength}-${passwordInput.maxLength} characters; 
+      you entered ${passwordInput.value.length}.`;
+    } else if (passwordInput.validity.patternMismatch) {
+      passwordError.textContent =
+        "The password must contain at least one digit, one uppercase letter and one special character.";
+    }
+    passwordError.classList.add("active");
+  };
+};
+
+export default passwordValidation;
