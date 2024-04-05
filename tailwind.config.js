ae036c01c0c13aa955d66814d6598cfa61a1b5cf
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-04-05 18:47:15 +0300

    Add footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,15 @@ module.exports = {
       textShadow: {
         default: "1px 1px 1px #000000",
       },
+      keyframes: {
+        rotateAndScale: {
+          "0%": { opacity: "0", transform: "rotate(0) scale(1)" },
+          "100%": { opacity: "1", transform: "rotate(360deg) scale(1.5)" },
+        },
+      },
+      animation: {
+        "rotate-and-scale": "rotateAndScale 500ms ease-in-out forwards",
+      },
     },
   },
   plugins: [require("tailwindcss-textshadow")],
